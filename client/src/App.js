7de32be31ee5d40dbd5091d9122bd16c4c4510db
SJ <vince15dk@gmail.com>
2019-07-07 12:00:23 +0900

    test

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator ON KUBERNETES With Travis CI!</h1>
+            <h1 className="App-title">Fib Calculator ON KUBERNETES With Travis CI</h1>
             <Link to="/">SJ HOME</Link>
             <Link to="/otherpage">Other Page</Link>
           </header>
